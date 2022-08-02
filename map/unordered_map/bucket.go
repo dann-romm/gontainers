5c3a2c09574b58a3f71b53ff64adb30f369d11b4
dann-romm <danilka.2002.lol@gmail.com>
2022-08-02 12:11:16 +0300

    unordered map functions added

--- map/unordered_map/bucket.go
+++ map/unordered_map/bucket.go
@@ -1,30 +1,30 @@
 package unordered_map
 
 type bucket[K comparable, V any] struct {
+	next     *bucket[K, V]
 	reserved uint64 // 0: empty, 1: occupied
 	bitmask  uint64
 	keys     [8]K
 	values   [8]V
-	next     *bucket[K, V]
 }
 
 func newEmptyBucket[K comparable, V any]() *bucket[K, V] {
 	return &bucket[K, V]{
+		next:     nil,
 		reserved: 0,
 		bitmask:  0,
 		keys:     [8]K{},
 		values:   [8]V{},
-		next:     nil,
 	}
 }
 
 func newBucket[K comparable, V any](key K, value V, hash uint64) *bucket[K, V] {
 	return &bucket[K, V]{
+		next:     nil,
 		reserved: 1,
 		bitmask:  hash & ((1 << 8) - 1),
 		keys:     [8]K{key},
 		values:   [8]V{value},
-		next:     nil,
 	}
 }
 
@@ -44,12 +44,13 @@ func (b *bucket[K, V]) get(key K, hash uint64) (*V, bool) {
 	return b.next.get(key, hash)
 }
 
-func (b *bucket[K, V]) set(key K, value V, hash uint64) {
+func (b *bucket[K, V]) set(key K, value V, hash uint64) bool {
 	if valuePtr, ok := b.get(key, hash); ok {
 		*valuePtr = value
-		return
+		return false
 	}
 	b.setNew(key, value, hash)
+	return true
 }
 
 func (b *bucket[K, V]) setNew(key K, value V, hash uint64) {
@@ -72,19 +73,51 @@ func (b *bucket[K, V]) setNew(key K, value V, hash uint64) {
 	b.next.setNew(key, value, hash)
 }
 
-func (b *bucket[K, V]) remove(key K, hash uint64) {
+func (b *bucket[K, V]) remove(key K, hash uint64) bool {
 	var mask uint64 = (1 << 8) - 1
 
 	for i := 0; i < 8; i++ {
 		if b.reserved&mask == 1 && b.bitmask&mask == hash&mask && b.keys[i] == key {
 			b.reserved &= ^mask
 			b.bitmask &= ^mask
-			return
+			return true
 		}
 		mask <<= 8
 	}
 	if b.next == nil {
-		return
+		return false
 	}
-	b.next.remove(key, hash)
+	return b.next.remove(key, hash)
+}
+
+func (b *bucket[K, V]) writeKeys(keys []K, index int) int {
+	var mask uint64 = (1 << 8) - 1
+
+	for i := 0; i < 8; i++ {
+		if b.reserved&mask == 1 {
+			keys[index] = b.keys[i]
+			index++
+		}
+		mask <<= 8
+	}
+	if b.next == nil {
+		return index
+	}
+	return b.next.writeKeys(keys, index)
+}
+
+func (b *bucket[K, V]) writeValues(values []V, index int) int {
+	var mask uint64 = (1 << 8) - 1
+
+	for i := 0; i < 8; i++ {
+		if b.reserved&mask == 1 {
+			values[index] = b.values[i]
+			index++
+		}
+		mask <<= 8
+	}
+	if b.next == nil {
+		return index
+	}
+	return b.next.writeValues(values, index)
 }
--- map/unordered_map/unordered_map.go
+++ map/unordered_map/unordered_map.go
@@ -1,30 +1,39 @@
 package unordered_map
 
+import (
+	"gontainers/container"
+	_map "gontainers/map"
+)
+
 type HashMap[K comparable, V any] struct {
-	cap        int
-	len        int
-	reserved   int
-	loadFactor float64
+	cap int
+	len int
 
 	hashFunc func(K) uint64
 	table    []*bucket[K, V]
 }
 
+// assert HashMap[K,V] to be a container.Container[V]
+var _ container.Container[int] = (*HashMap[int, int])(nil)
+
+// assert HashMap[K,V] to be a _map.Map[K,V]
+var _ _map.Map[int, int] = (*HashMap[int, int])(nil)
+
 const (
-	defaultLoadFactor = 0.75
-	defaultCapacity   = 16
+	loadFactorNumerator   = 13
+	loadFactorDenominator = 2
+
+	defaultCapacity = 16
 )
 
 func New[K comparable, V any]() *HashMap[K, V] {
 	capacity := defaultCapacity
 
 	return &HashMap[K, V]{
-		cap:        capacity,
-		len:        0,
-		reserved:   0,
-		loadFactor: defaultLoadFactor,
-		table:      newTable[K, V](capacity),
-		hashFunc:   newHashFunc[K](),
+		cap:      capacity,
+		len:      0,
+		table:    newTable[K, V](capacity),
+		hashFunc: newHashFunc[K](),
 	}
 }
 
@@ -34,4 +43,86 @@ func newTable[K comparable, V any](size int) []*bucket[K, V] {
 		table[i] = newEmptyBucket[K, V]()
 	}
 	return table
+}
+
+func (h *HashMap[K, V]) Set(key K, value V) {
+	hash := h.hashFunc(key)
+
+	b := h.table[hash%uint64(h.cap)]
+	if b.set(key, value, hash) {
+		h.len++
+		if h.len > h.cap*loadFactorNumerator/loadFactorDenominator {
+			// TODO: rehash here
+			// h.resize()
+		}
+	}
+	panic("not implemented")
+}
+
+func (h *HashMap[K, V]) Get(key K) (V, bool) {
+	hash := h.hashFunc(key)
+
+	b := h.table[hash%uint64(h.cap)]
+	valuePtr, ok := b.get(key, hash)
+	return *valuePtr, ok
+}
+
+func (h *HashMap[K, V]) Remove(key K) {
+	hash := h.hashFunc(key)
+
+	b := h.table[hash%uint64(h.cap)]
+	if b.remove(key, hash) {
+		h.len--
+	}
+}
+
+func (h *HashMap[K, V]) resize() {
+	table := newTable[K, V](h.cap * 2)
+	_ = table
+	for _, b := range h.table {
+		_ = b
+		// TODO: implement rehash
+	}
+	panic("not implemented")
+}
+
+func (h *HashMap[K, V]) Len() int {
+	return h.len
+}
+
+func (h *HashMap[K, V]) IsEmpty() bool {
+	return h.len == 0
+}
+
+func (h *HashMap[K, V]) Keys() []K {
+	keys := make([]K, h.len)
+	index := 0
+
+	for i := 0; i < h.cap; i++ {
+		index = h.table[i].writeKeys(keys, index)
+	}
+	return keys
+}
+
+func (h *HashMap[K, V]) Values() []V {
+	values := make([]V, h.len)
+	index := 0
+
+	for i := 0; i < h.cap; i++ {
+		index = h.table[i].writeValues(values, index)
+	}
+	return values
+}
+
+func (h *HashMap[K, V]) Clear() {
+	h.len = 0
+	for i := 0; i < h.cap; i++ {
+		h.table[i] = newEmptyBucket[K, V]()
+	}
+	h.hashFunc = newHashFunc[K]()
+}
+
+func (h *HashMap[K, V]) String() string {
+	panic("not implemented")
+	// TODO: implement this
 }
